5dfb7b07f9bfae44757c0e33f3a79e406de80e81
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-09-19 21:02:16 +0700

    update database

---
+++ DAO/Migrations/20240919140145_updatePromotion.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAO.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePromotion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActionPromotion",
+                table: "Promotion");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountTypeId",
+                table: "Promotion");
+
+            migrationBuilder.DropColumn(
+                name: "PromotionTypeId",
+                table: "Promotion");
+
+            migrationBuilder.RenameColumn(
+                name: "PromotionPack",
+                table: "Promotion",
+                newName: "DiscountTypeName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiscountTypeName",
+                table: "Promotion",
+                newName: "PromotionPack");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ActionPromotion",
+                table: "Promotion",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountTypeId",
+                table: "Promotion",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PromotionTypeId",
+                table: "Promotion",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
