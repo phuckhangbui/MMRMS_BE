e83eb374fe2d49c0a5a5c0f4ef0bd67f0485a6b2
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-09-19 20:56:17 +0700

    config mail service

---
+++ Service/Implement/MailService.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using Service.Interface;
+using Service.Mail;
+
+namespace Service.Implement
+{
+    public class MailService : IMailService
+    {
+        private MailSetting _mailSetting { get; set; }
+        public MailService(IOptions<MailSetting> options)
+        {
+            _mailSetting = options.Value;
+        }
+
+        public void SendMail(MailContent mailContent)
+        {
+            var email = new MimeMessage();
+            email.Sender = new MailboxAddress(_mailSetting.DisplayName, _mailSetting.Mail);
+            email.From.Add(new MailboxAddress(_mailSetting.DisplayName, _mailSetting.Mail));
+            email.To.Add(new MailboxAddress(mailContent.To, mailContent.To));
+            email.Subject = mailContent.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailContent.Body;
+            email.Body = builder.ToMessageBody();
+
+            using var smtpClient = new MailKit.Net.Smtp.SmtpClient();
+            try
+            {
+                smtpClient.Connect(_mailSetting.Host, _mailSetting.Port, SecureSocketOptions.StartTls);
+                smtpClient.Authenticate(_mailSetting.Mail, _mailSetting.Password);
+                smtpClient.Send(email);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            smtpClient.Disconnect(true);
+        }
+
+
+    }
+}
---
+++ Service/Interface/IMailService.cs
@@ -0,0 +1,10 @@
+﻿
+using Service.Mail;
+
+namespace Service.Interface
+{
+    public interface IMailService
+    {
+        void SendMail(MailContent mailContent);
+    }
+}
