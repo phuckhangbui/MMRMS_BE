c5a59ca298c84ad127954aaf9029780f1350d0e1
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-09-24 11:23:47 +0700

    update invoice table

--- BusinessObject/Invoice.cs
+++ BusinessObject/Invoice.cs
@@ -6,15 +6,23 @@ public partial class Invoice
 
     public string? InvoiceCode { get; set; }
 
+    public int? AccountPaidId { get; set; }
+
     public string? ContractId { get; set; }
 
-    public int? Method { get; set; }
+    public string? PaymentMethod { get; set; }
 
-    public double? Price { get; set; }
+    public double? Amount { get; set; }
 
     public DateTime? DateCreate { get; set; }
 
-    public bool? IsDelete { get; set; }
+    public string? Status { get; set; }
+
+    public string? Type { get; set; }
+
+    public string? Note { get; set; }
 
     public virtual Contract? Contract { get; set; }
+
+    public virtual Account? AccountPaid { get; set; }
 }
