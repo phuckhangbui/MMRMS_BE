467eadeab6430c1ca0c3677b2df71266e706d0c6
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-09-24 10:10:54 +0700

    add migration editPropertiesForSerialNumberProductTable

---
+++ DAO/Migrations/20240924031031_editPropertiesForSerialNumberProductTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAO.Migrations
+{
+    /// <inheritdoc />
+    public partial class editPropertiesForSerialNumberProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "ProductComponentStatus",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "ProductComponentStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "ProductComponentStatus");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "ProductComponentStatus",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
