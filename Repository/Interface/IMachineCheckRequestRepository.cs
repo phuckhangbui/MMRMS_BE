8cac3a5d86da4f5664e09a3ae3b315d34239a49a
khoalnd <khoaly090141@gmail.com>
2024-11-02 17:10:54 +0700

    add machine check criteria manage funcs

--- Repository/Interface/IMachineCheckRequestRepository.cs
+++ Repository/Interface/IMachineCheckRequestRepository.cs
@@ -6,6 +6,9 @@ namespace Repository.Interface
     {
         Task CreateMachineCheckRequest(int customerId, CreateMachineCheckRequestDto createMachineCheckRequestDto);
         Task<IEnumerable<MachineCheckCriteriaDto>> GetMachineCheckCriteriaList();
+        Task CreateMachineCheckCriteria(string name);
+        Task<bool> UpdateMachineCheckCriteria(int id, string name);
+        Task<bool> DeleteMachineCheckCriteria(int id);
         Task<MachineCheckRequestDto> GetMachineCheckRequest(string machineCheckRequestId);
         Task<MachineCheckRequestDetailDto> GetMachineCheckRequestDetail(string machineCheckRequestId);
         Task<IEnumerable<MachineCheckRequestDto>> GetMachineCheckRequests();
--- Service/Interface/IMachineCheckRequestService.cs
+++ Service/Interface/IMachineCheckRequestService.cs
@@ -7,6 +7,9 @@ namespace Service.Interface
         Task CancelMachineCheckRequestDetail(string machineCheckRequestId, int customerId);
         Task CreateMachineCheckRequest(int customerId, CreateMachineCheckRequestDto createMachineCheckRequestDto);
         Task<IEnumerable<MachineCheckCriteriaDto>> GetMachineCheckCriterias();
+        Task CreateMachineCheckCriteria(string name);
+        Task<bool> UpdateMachineCheckCriteria(int id, string name);
+        Task<bool> DeleteMachineCheckCriteria(int id);
         Task<MachineCheckRequestDetailDto> GetMachineCheckRequestDetail(string machineCheckRequestId);
         Task<IEnumerable<MachineCheckRequestDto>> GetMachineCheckRequests();
         Task<IEnumerable<MachineCheckRequestDto>> GetMachineCheckRequests(int customerId);
