456571932e14360b9224e5f58407ad8e648af946
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-11-12 12:26:48 +0700

    add api update contract delivery status

--- Service/Interface/IDeliveryService.cs
+++ Service/Interface/IDeliveryService.cs
@@ -10,6 +10,7 @@ namespace Service.Interface
         Task<DeliveryTaskDetailDto> GetDeliveryDetail(int deliveryTaskId);
         Task StaffCompleteDelivery(StaffUpdateDeliveryTaskDto staffUpdateDeliveryTaskDto, int accountId);
         Task StaffFailDelivery(StaffFailDeliveryTaskDto staffFailDeliveryTask, int accountId);
+        Task UpdateContractDeliveryStatusToProcessedAfterFailure(int contractDeliveryId, int accountId);
         Task UpdateDeliveryStatusToDelivering(int deliveryTaskId, int accountId);
         Task UpdateDeliveryStatusToProcessedAfterFailure(int deliveryTaskId, int accountId);
         //Task UpdateDeliveryTaskStatus(int DeliveryTaskId, string status, int accountId);
