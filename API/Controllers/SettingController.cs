03abfa1a79aaf63168f50dee40b0579e2b75403c
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-11-17 16:29:31 +0700

    add setting function for admin

---
+++ API/Controllers/SettingController.cs
@@ -0,0 +1,59 @@
+﻿using DTOs.Setting;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Exceptions;
+using Service.Interface;
+
+namespace API.Controllers
+{
+    [Route("api/settings")]
+    public class SettingsController : BaseApiController
+    {
+        private readonly ISettingsService _settingsService;
+
+        public SettingsController(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SettingDto>>> GetSettings()
+        {
+            try
+            {
+                var settings = await _settingsService.GetSettingsAsync();
+                return Ok(settings);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "Admin")]
+        public async Task<ActionResult> UpdateSetting(UpdateSettingDto updateSettingDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errorMessages = ModelStateValidation.GetValidationErrors(ModelState);
+                return BadRequest(errorMessages);
+            }
+
+            try
+            {
+                await _settingsService.UpdateSettingAsync(updateSettingDto);
+                return Ok("Setting updated successfully.");
+            }
+            catch (ServiceException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+
+}
---
+++ Service/Interface/ISettingService.cs
@@ -0,0 +1,11 @@
+﻿using DTOs.Setting;
+
+namespace Service.Interface
+{
+    public interface ISettingsService
+    {
+        Task<IEnumerable<SettingDto>> GetSettingsAsync();
+        Task UpdateSettingAsync(UpdateSettingDto updateSettingDto);
+    }
+
+}
