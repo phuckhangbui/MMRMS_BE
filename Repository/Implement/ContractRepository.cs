f444a11c35f0dd14005736acb2fbfd7f992e81f7
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-11-28 10:36:45 +0700

    hot fix function UpdateContractStatusToRenting

--- Repository/Implement/ContractRepository.cs
+++ Repository/Implement/ContractRepository.cs
@@ -139,7 +139,7 @@ namespace Repository.Implement
 
             var oldDeposit = contract.DepositPrice;
 
-            contract.Status = ContractStatusEnum.Signed.ToString();
+            contract.Status = ContractStatusEnum.Renting.ToString();
             if (deliveryRefund > 0)
             {
                 contract.RefundShippingPrice = deliveryRefund;
